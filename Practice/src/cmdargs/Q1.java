d1fbf21550bf2eef5ff22165a09ce35df4a1e8a8
charitha <saicharithan@gmail.com>
2021-05-21 19:42:16 +0530

    Practice on command line arguments

---
+++ Practice/src/cmdargs/Q1.java
@@ -0,0 +1,10 @@
+package cmdargs;
+
+public class Q1 {
+	public static void main(String args[])
+	{
+		
+		System.out.println(args[0]+" "+"Technologies"+" "+args[1]);
+	}
+
+}
---
+++ Practice/src/cmdargs/Q3.java
@@ -0,0 +1,12 @@
+package cmdargs;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("The sum of "+a+" and "+b+" is "+(a+b));
+	}
+
+}
