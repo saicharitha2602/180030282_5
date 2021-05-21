515e7acac3f319ef6085f9d021599d74c3cd87f6
charitha <saicharithan@gmail.com>
2021-05-21 19:58:51 +0530

    Interface Problem 2

---
+++ Practice/src/interfaces/music/wind/Saxophone.java
@@ -0,0 +1,12 @@
+package interfaces.music.wind;
+
+import interfaces.music.Playable;
+public class Saxophone implements Playable {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Playing  Saxophone....");
+	}
+
+}
